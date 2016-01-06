25fb1d52c92554fea1325ecc42ea005e3ced8534
Vicente Bermúdez <stealyerms@gmail.com>
2016-01-06 11:26:56 -0300

    Fix identation

--- js/searchController.js
+++ js/searchController.js
@@ -1,7 +1,7 @@
 var app = angular.module('searchController', []);
 var baseUrl;
 var manifest = jQuery.getJSON( "manifest.webapp", function( json ) {
-    baseUrl = json.activities.openmrs.href;
+	baseUrl = json.activities.openmrs.href;
 });
 
 app.controller('SearchConceptCtrl', function($scope, $http, $window) {
@@ -11,9 +11,9 @@ app.controller('SearchConceptCtrl', function($scope, $http, $window) {
 	$scope.searchConcept = function() {
 		$http({ method: 'GET', url: baseUrl + "/ws/rest/v1/" + 'concept?q=' + $scope.query + '&v=custom:(id,name,description)' }).
 			success(function (data, status, headers, config) {
-		    	$scope.concepts = data.results;
-		    	$scope.showing = $scope.query;
-		    	console.log('REST success');
+		    		$scope.concepts = data.results;
+		    		$scope.showing = $scope.query;
+		    		console.log('REST success');
 		  	}).
 		  	error(function (data, status, headers, config) {
 		   		console.log('REST failure');
@@ -21,6 +21,6 @@ app.controller('SearchConceptCtrl', function($scope, $http, $window) {
 	};
 
 	$scope.redirectToDictionary = function(id) {
-	    $window.open(baseUrl + '/dictionary/concept.htm?conceptId=' + id, '_blank');
+		$window.open(baseUrl + '/dictionary/concept.htm?conceptId=' + id, '_blank');
 	};
 });
